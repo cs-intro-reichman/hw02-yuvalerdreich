5bdb2ed1fe389268997f56dad6dda3c6debe6aee
yuvalerdreich <154261645+yuvalerdreich@users.noreply.github.com>
2024-01-04 00:43:45 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -2,17 +2,16 @@ public class DamkaBoard {
 	public static void main(String[] args) {
 		
 		int BoardSize = Integer.parseInt(args[0]);
-		String Stars = " ";
-		for (int i = 0; i < BoardSize; i++) {
-			Stars += "*";
+
+        for (int i = 0; i < BoardSize; i++) {
+            for (int j = 0; j < BoardSize; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print(" * ");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
 		}
-		
-		for (int i = 0; i < BoardSize; i++) {
-            if (i % 2 != 0) {
-                System.out.println(Stars);
-            } else {
-                System.out.println(" " + Stars);
-			}
-        }
 	}
-}		
+}	
