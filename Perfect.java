66da6be716013ca94ff171ca827bd6951994aed5
yuvalerdreich <154261645+yuvalerdreich@users.noreply.github.com>
2024-01-03 23:25:08 +0200

    Delete Perfect.java

--- Perfect.java
+++
@@ -1,8 +0,0 @@
-/**
- *  Gets a command-line argument (int), and chekcs if the given number is perfect.
- */
-public class Perfect {
-	public static void main (String[] args) {
-		//// Put your code here
-	}
-}
