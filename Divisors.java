8e883cc05eb5c295ea58787bcfc4802bc2cb5273
yuvalerdreich <154261645+yuvalerdreich@users.noreply.github.com>
2024-01-03 23:26:28 +0200

    Delete Divisors.java

--- Divisors.java
+++
@@ -1,8 +0,0 @@
-/**
- *  Gets a command-line argument (int), and prints all the divisors of the given number.
- */
-public class Divisors {
-	public static void main (String[] args) {
-		//// Put your code here
-	}
-}
