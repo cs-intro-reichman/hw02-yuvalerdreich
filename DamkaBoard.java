34bfc3802e9f9fd7085deaf8826ac586a353e4cf
yuvalerdreich <154261645+yuvalerdreich@users.noreply.github.com>
2024-01-03 23:26:18 +0200

    Delete DamkaBoard.java

--- DamkaBoard.java
+++
@@ -1,8 +0,0 @@
-/**
- *  Gets a command-line argument n (int), and prints an n-by-n damka board.
- */
-public class DamkaBoard {
-	public static void main(String[] args) {
-		//// Put your code here
-	}
-}
