ca0d1c786e2126dcd80b8449d30ce3d2f0f057b6
yuvalerdreich <154261645+yuvalerdreich@users.noreply.github.com>
2024-01-04 08:30:28 +0200

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -8,7 +8,7 @@ public class Reverse {
 			System.out.print(s.charAt(i));
 			i--;
 		}
-		int middleChar = length / 2;
+		int middleChar = length / 2 - 1;
 		System.out.println("\nThe middle character is " + s.charAt(middleChar));
 	}
 }
